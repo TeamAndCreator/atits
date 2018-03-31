46f0724ccbb20fabc56ee7110ab9b220f8d04fb5
yuxiangfeng <1220453059@qq.com>
2018-03-31 21:40:27 +0800

    修复了重大文件报告，500错误未解决

--- src/main/java/com/atits/controller/MainController.java
+++ src/main/java/com/atits/controller/MainController.java
@@ -38,6 +38,9 @@ public class MainController {
     private FilesService filesService;
 
     @Resource
+    private ReportService reportService;
+
+    @Resource
     private RegulationService regulationService;// 注入业务层
 
     @Resource
@@ -66,6 +69,9 @@ public class MainController {
 
         List<Activity> activitys = this.activityService.findAll();
         model.addAttribute("activitys", activitys);
+
+        List<Report> reports = this.reportService.findAll();
+        model.addAttribute("reports", reports);
         return "main";
     }
 
@@ -187,50 +193,36 @@ public class MainController {
         return "main_activity";
     }
 
-//    /**
-//     * 文件下载
-//     *
-//     * @param id
-//     * @param response
-//     * @throws IOException
-//     */
-//    @RequestMapping(value = "/files_download/{id}", method = RequestMethod.GET)
-//    public void download(@PathVariable("id") Integer id, HttpServletResponse response) throws IOException {
-//        Files files = filesService.findById(id);
-//
-//        String filePath = FILE_PATH + files.getSystem().getId() + "/" + files.getFileType() + "/" + files.getEditor().getId() + "/" + files.getFileName();
-//        java.lang.System.out.println("file:"+filePath);
-//        File file = new File(filePath);
-//        if (file.exists()) {
-//            String filename = URLEncoder.encode(files.getFileName(), "UTF-8");
-//            //response.setContentType("application/pdf");
-//            response.setHeader("Content-disposition", "attachment;filename=" + filename);
-//            byte[] buffer = new byte[1024];
-//            FileInputStream fis = null;
-//            BufferedInputStream bis = null;
-//            fis = new FileInputStream(file);
-//            bis = new BufferedInputStream(fis);
-//            OutputStream os = response.getOutputStream();
-//            int i = bis.read(buffer);
-//            while (i != -1) {
-//                os.write(buffer, 0, i);
-//                i = bis.read(buffer);
-//            }
-//            os.flush();
-//            os.close();
-//            bis.close();
-//            fis.close();
-//
-//        } else {
-//            response.setContentType("text/html");
-//            response.setCharacterEncoding("UTF-8");
-//            PrintWriter pwt = response.getWriter();
-//            pwt.println("文件不存在");
-//            pwt.close();
-//        }
-//
-//    }
-//
+
+    /**
+     * 查看详情,重大文件
+     *
+     * @param id
+     * @param model
+     * @return
+     */
+
+    @RequestMapping(value = "/main_report", method = RequestMethod.GET)
+    public String findById5(@RequestParam("id") Integer id, Model model) {
+        Report report = reportService.findById(id);
+
+        if (report.getFileId() != "") {
+            String[] temp = report.getFileId().split(",");// 以逗号拆分字符串
+            Integer[] ids = new Integer[temp.length];// int类型数组
+            for (int i = 0; i < temp.length; i++) {
+                ids[i] = Integer.parseInt(temp[i]);// 整数数组
+            }
+            List<Files> files = filesService.findByIds(ids);
+            if (files.isEmpty()) {
+                files = null;
+            }
+            model.addAttribute("files", files);
+        }
+
+        model.addAttribute("report", report);
+        return "main_report";
+    }
+
     /**
      * 查看详情：规章制度
      *
